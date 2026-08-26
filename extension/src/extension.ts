2007a7d820b43fca2b32255f426fce3a606846c3
Benjamin <189326784+Zomt3c@users.noreply.github.com>
2026-08-26 20:12:39 +0000

    feat(extension): add an o3s activity bar view and scope activation

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,11 +1,24 @@
 import * as vscode from "vscode";
 
+class MainViewProvider implements vscode.WebviewViewProvider {
+  resolveWebviewView(webviewView: vscode.WebviewView) {
+    webviewView.webview.options = { enableScripts: false };
+    webviewView.webview.html = /* html */ `<!DOCTYPE html>
+<html>
+  <body></body>
+</html>`;
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand("o3s.generateDevcontainer", () => {
       vscode.window.showInformationMessage("o3s: devcontainer.json generation not implemented yet.");
-    })
+    }),
+    vscode.window.registerWebviewViewProvider("o3s.mainView", new MainViewProvider())
   );
+
+  vscode.commands.executeCommand("workbench.view.extension.o3s");
 }
 
 export function deactivate() {}
